2de6a0fdc8ddb9f7cf3adce8cff6eb15d9fdf3b3
Alan Tse <starttodevelop@gmail.com>
2017-03-16 15:07:32 +1100

    Added input from keyboard

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Program {
 	public static Person person;
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter your name:");
+		String myName = input.nextLine();
+		//System.out.println("Welcome " + myName);
+		
+		System.out.println("Enter your gender:");
+		String myGender = input.nextLine();
+		
+		System.out.println("Enter your age:");
+		int myAge = input.nextInt();
+		//System.out.println("Your age is " + myAge);
 		// TODO Auto-generated method stub
 		System.out.println("Program started...");
 		person = new Person(46);
@@ -12,6 +25,7 @@ public class Program {
 		System.out.println(person.getName());
 		
 		person.runs();
-		//System.out.printf(format, args)
+		
+		System.out.printf("%s is %s who is %s years old", myName, myGender, myAge);
 	}
 }
